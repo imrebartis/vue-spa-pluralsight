3de9e7b67645f2865c75fb49241060efe868b141
Imre Bartis <imre.bartis@siili.com>
2018-02-21 13:02:37 +0200

    router-link & scrollBehavior

--- src/router.js
+++ src/router.js
@@ -6,6 +6,20 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
+  // scrollBehavior (to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition
+  //   }
+  // },
+  // scrollBehavior (to, from, savedPosition) {
+  //   if (to.hash) {
+  //     return {
+  //       selector: to.hash
+  //     }
+  //   }
+  // },
   routes: [
     { path: '/login', component: Login },
     { path: '/', component: Category }
