54b2d1ddc3cc06e4677770dc48ca450ed15ee96a
Imre Bartis <imre.bartis@siili.com>
2018-02-21 09:59:29 +0200

    .vue files

---
+++ src/theme/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+        <nav class="nav has-shadow">
+          <div class="container">
+            <a href="/">
+              <img src="http://bit.ly/vue-img"
+              alt="Vue SPA" />
+            </a>
+          </div>
+        </nav>
+        <section class="main-section section"></section>
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              Follow us on le
+              <a href="https://twitter.com/i_m_bjim" target="_blank">Twitter</a>
+            </div>
+          </div>
+        </footer>
+  </div>
+</template>
+<script>
+export default{}
+</script>
