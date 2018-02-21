4612911e017c80e5d950b97aae5f5aa089b1178c
Imre Bartis <imre.bartis@siili.com>
2018-02-21 10:08:17 +0200

    ES6 transpiling

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import AppLayout from './theme/Layout.vue'
 
+console.log(AppLayout)
+
 const app = new Vue({
-  render: h => h(AppLayout)
+  ...AppLayout
 })
 
 export { app }
