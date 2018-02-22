9449844d017235043baf7ead055ff73aa4e848be
Imre Bartis <imre.bartis@siili.com>
2018-02-22 14:36:59 +0200

    triggering actions

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import appService from '../app.service.js'
 
 Vue.use(Vuex)
 
@@ -17,18 +18,45 @@ const store = new Vuex.Store({
   actions: {
     logout (context) {
       context.commit('logout')
+    },
+    login (context, credentials) {
+      return new Promise((resolve) => {
+        appService.login(credentials)
+          .then((data) => {
+            context.commit('login', data)
+
+            resolve()
+          })
+          .catch(() => window.alert('Could not login!'))
+      })
     }
   },
   mutations: {
     logout (state) {
-      state.isAuthenticated = false
       if (typeof window !== 'undefined') {
         window.localStorage.setItem('token', null)
         window.localStorage.setItem('tokenExpiration', null)
-        this.isAuthenticated = false
       }
+      state.isAuthenticated = false
+    },
+    login (state, token) {
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem('token', token.token)
+        window.localStorage.setItem('tokenExpiration', token.expiration)
+      }
+      state.isAuthenticated = true
     }
   }
 })
+
+if (typeof window !== 'undefined') {
+  document.addEventListener('DOMContentLoaded', function (event) {
+    let expiration = window.localStorage.getItem('tokenExpiration')
+    var unixTimestamp = new Date().getTime() / 1000
+    if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
+      store.state.isAuthenticated = true
+    }
+  })
+}
 
 export default store
