24a816e9c84f0a100cab566541e4050ce137f0ad
Imre Bartis <imre.bartis@siili.com>
2018-02-22 10:05:24 +0200

    JSON Web Tokens (JWT)

--- src/app.service.js
+++ src/app.service.js
@@ -2,15 +2,6 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.fullstackweekly.com'
 
-// axios.interceptors.request.use(function (config) {
-//   const token = window.localStorage.getItem('token')
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-
-//   return config
-// })
-
 const appService = {
   getPosts (categoryId) {
     return new Promise((resolve) => {
@@ -19,25 +10,17 @@ const appService = {
           resolve(response.data)
         })
     })
+  },
+  login (credentials) {
+    return new Promise((resolve, reject) => {
+      axios.post('/services/auth.php', credentials)
+        .then(response => {
+          resolve(response.data)
+        }).catch(response => {
+          reject(response.status)
+        })
+    })
   }
-  // getProfile () {
-  //   return new Promise((resolve) => {
-  //     axios.get('/services/profile.php')
-  //       .then(response => {
-  //         resolve(response.data)
-  //       })
-  //   })
-  // },
-  // login (credentials) {
-  //   return new Promise((resolve, reject) => {
-  //     axios.post('/services/auth.php', credentials)
-  //       .then(response => {
-  //         resolve(response.data)
-  //       }).catch(response => {
-  //         reject(response.status)
-  //       })
-  //   })
-  // }
 }
 
 export default appService
